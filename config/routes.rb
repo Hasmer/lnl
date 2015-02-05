d11ca5cb0aadc054cf9552b1a6aaa264321bdb91
Hasmer <trancephere@mail.ru>
2015-02-05 19:22:00 +0200

    Add help page and footer

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
     root 'static_pages#home'
     get 'home' => 'static_pages#home'
     get 'help' => 'static_pages#help'
+    get 'about' => 'static_pages#about'
   end
 
   # The priority is based upon order of creation: first created -> highest priority.
