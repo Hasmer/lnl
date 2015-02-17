1ead2580ee26d5f7b8639cd025e12eb31f7fde2f
Hasmer <trancephere@mail.ru>
2015-02-17 19:21:34 +0200

    Add creating new users; add ru locales for some errors

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,15 @@
 class UsersController < ApplicationController
   def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to @user
+    else
+      render 'new'
+    end
   end
 
   def show
@@ -9,4 +19,9 @@ class UsersController < ApplicationController
     @user.name = "User doesn't exist"
     @user.email = "example@railstutorial.org"
   end
+
+  private
+    def user_params
+      params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    end
 end
